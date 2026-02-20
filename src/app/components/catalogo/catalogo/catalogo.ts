d1486c9ae046bc71baf22145eeb753d6b4fb33be
7Abyl7 <195319778+7Abyl7@users.noreply.github.com>
2026-02-20 08:04:29 -0600

    Se despliegan los fondos en la pagina, creo

--- src/app/components/catalogo/catalogo/catalogo.ts
+++ src/app/components/catalogo/catalogo/catalogo.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Product } from '../../../models/producto.model';
 import { ProductCardComponent } from '../product-card/product-card';
 import { ProductsService } from '../../../services/productos.service';
 
 @Component({
   selector: 'app-catalogo',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './catalogo.html',
   styleUrl: './catalogo.css',
 })
-export class CatalogoComponent {
-  products:Product[] = [];
-  constructor(private productsService:ProductsService) {
-    this.products = this.productsService.getAll();
+export class CatalogoComponent implements OnInit {
+  products$ = this.productsService.getAll$();
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    // Los productos se cargan automáticamente desde el Observable
   }
 }

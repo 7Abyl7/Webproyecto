4646750556623999d40f77a61064d5fe86aae72b
a22300899-gif <a22300899@ceti.mx>
2026-03-11 13:31:46 -0600

    max estuvo aqui

--- src/app/components/catalogo/catalogo/catalogo.ts
+++ src/app/components/catalogo/catalogo/catalogo.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Product } from '../../../models/producto.model';
 import { ProductCardComponent } from '../product-card/product-card';
+import { CarritoComponent } from '../carrito/carrito';
+import { CarritoService } from '../../../services/carrito.service';
 import { ProductsService } from '../../../services/productos.service';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, CarritoComponent],
   templateUrl: './catalogo.html',
   styleUrl: './catalogo.css',
 })
 export class CatalogoComponent {
-  products: Product[] = [];
+  products = signal<Product[]>([]);
 
-  constructor(private productsService: ProductsService) {
-    this.productsService.getAll().subscribe(data => {
-      this.products = data;
+  constructor(private productsService: ProductsService, private carritoService: CarritoService) {
+    this.productsService.getAll().subscribe({
+      next: (data) => this.products.set(data),
+      error: (err) => console.error('Error cargando XML:', err),
     });
+  }
+
+  agregar(producto: Product) {
+    this.carritoService.agregar(producto);
   }
 }

2f1c00c1ecc05f4f98d9c519b90956fcadc470d7
a22300899-gif <a22300899@ceti.mx>
2026-04-29 22:02:15 -0600

    Credenciales

--- src/app/components/catalogo/carrito/carrito.ts
+++ src/app/components/catalogo/carrito/carrito.ts
@@ -2,12 +2,13 @@ import { Component, computed } from '@angular/core';
 import { Product } from '../../../models/producto.model';
 import { CurrencyPipe } from '@angular/common';
 import { CarritoService } from '../../../services/carrito.service';
+import { Router, RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { Signal } from '@angular/core';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, RouterOutlet, RouterLinkWithHref],
   templateUrl: './carrito.html',
   styleUrl: './carrito.css',
 })
@@ -15,10 +16,23 @@ export class CarritoComponent {
   carrito: Signal<Product[]>;
   total = computed(() => this.carritoService.total());
 
+<<<<<<< Updated upstream
   constructor(private carritoService: CarritoService) {
     this.carrito = this.carritoService.productos;
   }
 
+=======
+  constructor(private carritoService: CarritoService, private cdr: ChangeDetectorRef, private router: Router) {
+    this.carrito = this.carritoService.productos;
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.cdr.detectChanges();
+    }, 0);
+  }
+
+>>>>>>> Stashed changes
   quitar(id: number) {
     this.carritoService.quitar(id);
   }
@@ -37,5 +51,9 @@ export class CarritoComponent {
 
   modificarCarrito(producto: Product) {
     this.carritoService.modificarCarrito(producto);
+  }
+
+  checkout() {
+    this.router.navigate(['checkout']);
   }
 }

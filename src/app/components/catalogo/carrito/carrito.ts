5572c4633ff70cc790f65fc8991631bfbe1086ea
a22300899-gif <a22300899@ceti.mx>
2026-04-29 22:51:57 -0600

    Revert "Merge pull request #1 from 7Abyl7/Paypal-Adrian"
    This reverts commit 0bf73961da4b346d4fd6950abbfd516c5bd50d3e, reversing
    changes made to 7dcc73e39800a5cc9b6d093257147aa31378928c.

--- src/app/components/catalogo/carrito/carrito.ts
+++ src/app/components/catalogo/carrito/carrito.ts
@@ -2,13 +2,12 @@ import { Component, computed } from '@angular/core';
 import { Product } from '../../../models/producto.model';
 import { CurrencyPipe } from '@angular/common';
 import { CarritoService } from '../../../services/carrito.service';
-import { Router, RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { Signal } from '@angular/core';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [CurrencyPipe, RouterOutlet, RouterLinkWithHref],
+  imports: [CurrencyPipe],
   templateUrl: './carrito.html',
   styleUrl: './carrito.css',
 })
@@ -16,23 +15,10 @@ export class CarritoComponent {
   carrito: Signal<Product[]>;
   total = computed(() => this.carritoService.total());
 
-<<<<<<< Updated upstream
   constructor(private carritoService: CarritoService) {
     this.carrito = this.carritoService.productos;
   }
 
-=======
-  constructor(private carritoService: CarritoService, private cdr: ChangeDetectorRef, private router: Router) {
-    this.carrito = this.carritoService.productos;
-  }
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.cdr.detectChanges();
-    }, 0);
-  }
-
->>>>>>> Stashed changes
   quitar(id: number) {
     this.carritoService.quitar(id);
   }
@@ -51,9 +37,5 @@ export class CarritoComponent {
 
   modificarCarrito(producto: Product) {
     this.carritoService.modificarCarrito(producto);
-  }
-
-  checkout() {
-    this.router.navigate(['checkout']);
   }
 }
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api',
-    paypalClientId: 'AVTYi-q1BRgng_Rf4MeXNnTQlSslp4tYKQgz-8fy4MOf8IK1Xck4BTJZ0QIEhmvo71sKu4C4m88Ova7F',
-    currency: 'MXN'
-};

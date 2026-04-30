f87fdaa60b7a962ca1672a23ed55a8f04c5e6093
a22300899-gif <a22300899@ceti.mx>
2026-04-29 22:38:53 -0600

    Merge 2

--- src/app/components/catalogo/carrito/carrito.ts
+++ src/app/components/catalogo/carrito/carrito.ts
@@ -2,12 +2,13 @@ import { Component, computed, ChangeDetectorRef, OnInit } from '@angular/core';
 import { Product } from '../../../models/producto.model';
 import { CurrencyPipe } from '@angular/common';
 import { CarritoService } from '../../../services/carrito.service';
+import { Router, RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { Signal } from '@angular/core';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, RouterOutlet, RouterLinkWithHref],
   templateUrl: './carrito.html',
   styleUrl: './carrito.css',
 })
@@ -15,7 +16,7 @@ export class CarritoComponent implements OnInit {
   carrito: Signal<Product[]>;
   total = computed(() => this.carritoService.total());
 
-  constructor(private carritoService: CarritoService, private cdr: ChangeDetectorRef) {
+  constructor(private carritoService: CarritoService, private cdr: ChangeDetectorRef, private router: Router) {
     this.carrito = this.carritoService.productos;
   }
 
@@ -43,5 +44,9 @@ export class CarritoComponent implements OnInit {
 
   modificarCarrito(producto: Product) {
     this.carritoService.modificarCarrito(producto);
+  }
+
+  checkout() {
+    this.router.navigate(['checkout']);
   }
 }

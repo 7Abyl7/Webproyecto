ec6b46e015226ee852a48b24980849fe9c4c60fd
a22300899-gif <a22300899@ceti.mx>
2026-04-29 22:44:22 -0600

    Merge 6

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000/api',
-    paypalClientId: 2,
+    paypalClientId: 'AVTYi-q1BRgng_Rf4MeXNnTQlSslp4tYKQgz-8fy4MOf8IK1Xck4BTJZ0QIEhmvo71sKu4C4m88Ova7F',
     currency: 'MXN'
 };
